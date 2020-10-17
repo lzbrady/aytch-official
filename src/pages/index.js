b87e8b07cba84491df0a7d6d48c3673e7afbb3b4
Luke Brady <lzbrady496@gmail.com>
2020-10-17 16:29:59 -0400

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react'
+
+import Layout from 'layouts/Layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Layout>Hello world!</Layout>
 }
